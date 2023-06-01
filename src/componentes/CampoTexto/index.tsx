a53d5f752947fd20718160693a7c3e018c219b84
saracsilva <saracarolinasilva@gmail.com>
2023-06-01 15:58:39 +0200

    transformed formulario component to TS

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -5,15 +5,15 @@ interface CampoTextoProps {
   placeholder: string;
   label: string;
   valor: string;
-  obrigatorio: boolean;
+  obrigatorio?: boolean;
 }
 
 const CampoTexto = ({
   aoAlterado,
   label,
-  obrigatorio,
   valor,
   placeholder,
+  obrigatorio = false,
 }: CampoTextoProps) => {
   const placeholderModificada = `${placeholder}...`;
 
